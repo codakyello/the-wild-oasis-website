832f475095c543d73b0f309830b85632332c04ac
Codakyello <olaoluwathompson8@gmail.com>
2024-10-05 03:30:47 +0100

    fix import

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
+import logo from "@/public/logo.png";
 
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
       <Image
-        src="/logo.png"
+        src={logo}
         quality={100}
         height="60"
         width="60"
