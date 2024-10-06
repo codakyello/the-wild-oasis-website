b5a6ef617bc6374710623ca0a61860e7ec809ac9
Codakyello <olaoluwathompson8@gmail.com>
2024-10-07 00:34:09 +0100

    the elegant escape

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -5,15 +5,14 @@ import logo from "@/public/icon-logo.png";
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-4 z-10">
-      {/* <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" /> */}
       <Image
         src={logo}
         className="h-[6rem] w-[6rem]"
         quality={100}
-        alt="The Wild Oasis logo"
+        alt="The Elegant Escape logo"
       />
       <span className="hidden md:flex text-xl font-semibold text-primary-100">
-        The Wild Oasis
+        The Elegant Escape
       </span>
     </Link>
   );
