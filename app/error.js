374d06abcc25bba5672c17a5c20d21ba98eeb6ce
Codakyello <olaoluwathompson8@gmail.com>
2024-10-07 10:45:45 +0100

    global error boundary

--- app/error.js
+++ app/error.js
@@ -4,9 +4,7 @@ export default function Error({ error, reset }) {
   return (
     <main className="flex justify-center items-center flex-col gap-6">
       <h1 className="text-[3rem] font-semibold">Something went wrong!</h1>
-      <p className="text-[1.8rem]">
-        Please try again later or refresh the page.
-      </p>
+      <p className="text-[1.8rem]">Please try again later.</p>
 
       <button
         onClick={reset}
